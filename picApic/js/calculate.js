c16023175ff4fc1f5e0d549d0c0661926474e0fa
yyyuanll <91445230+yyyuanll@users.noreply.github.com>
2021-10-08 21:44:27 +0800

    Add files via upload

---
+++ picApic/js/calculate.js
@@ -0,0 +1,125 @@
+//canvas图像的宽高 
+var c_w = 400; var c_h = 300;
+
+//加载img图像
+function loadImg(){
+ 	var img = document.getElementById("scream");
+ 	var file = document.querySelector('input[type=file]').files[0];
+ 	if(!/image\/\w+/.test(file.type)){
+ 		alert("文件必须为图片！");
+ 		return false;
+ 	}
+ 	var reader = new FileReader();
+	
+ 	reader.addEventListener("load", function () {img.src = reader.result;}, false);
+ 	if(file) {
+ 		reader.readAsDataURL(file);
+ 		loadCanvas(img);
+ 		}
+	
+	//计算图片的大小和像素
+	alert('图片大小为:'+file.size+'kb\n像素:'+img.width+' X '+img.height+'(px)');
+}
+//加载canvas图像
+function loadCanvas(img){
+ 		var c=document.getElementById("canvas");
+ 		var ctx=c.getContext("2d");
+		
+ 		img.onload = function() {
+ 		ctx.drawImage(img,0,0,c_w,c_h);
+ 		} 
+ }
+	
+
+//计算灰度（亮度、明度）
+function CalculateGray(){
+	var c=document.getElementById("canvas");  
+    var ctx=c.getContext("2d");
+	var imgData = ctx.getImageData(0,0,c_w,c_h);      //获取图像矩阵
+	var gray = 0;
+	for (var i = 0 ; i < imgData.data.length ; i+=4){      //遍历图像矩阵
+	   var R = imgData.data[i]; //R(0-255)
+	   var G = imgData.data[i+1]; //G(0-255)
+	   var B = imgData.data[i+2]; //G(0-255)
+	   var Alpha = imgData.data[i+3]; //Alpha(0-255)
+	   //浮点算法
+	   gray += R*0.299 + G*0.587 + B*0.114;
+	  }
+	window.alert(gray/imgData.data.length);
+  //document.getElementById("Brightness").innerHTML = "该图片的亮度值是：" + gray/imgData.data.length;
+}
+
+//计算透明度
+ function CalculateAlpha(){
+ 	 var c=document.getElementById("canvas");
+ 	 var ctx=c.getContext("2d");
+ 	 var imgData = ctx.getImageData(0,0,c_w,c_h);
+ 	 var sum = 0;
+ 	 for (var i = 0 ; i < imgData.data.length ; i+=4){
+ 	    var R = imgData.data[i]; //R(0-255)
+ 	    var G = imgData.data[i+1]; //G(0-255)
+ 	    var B = imgData.data[i+2]; //G(0-255)
+ 	    var Alpha = imgData.data[i+3]; //Alpha(0-255)
+	    sum += Alpha;
+ 	  }
+ 	  window.alert(sum/imgData.data.length);
+ 	 
+ }
+ 
+//将图像转为灰度图
+function Convert256toGray(){
+    var c=document.getElementById("canvas");
+    var ctx=c.getContext("2d");
+    var imgData = ctx.getImageData(0,0,c_w,c_h);
+    for (var i=0;i<imgData.data.length;i+=4){
+       var R = imgData.data[i]; //R(0-255)
+       var G = imgData.data[i+1]; //G(0-255)
+       var B = imgData.data[i+2]; //G(0-255)
+       var Alpha = imgData.data[i+3]; //Alpha(0-255)
+       //浮点算法
+       var gray = R*0.299 + G*0.587 + B*0.114;
+    //整数算法
+   //  var gray = (R*299 + G*587 + B*114 + 500) / 1000;　
+    //移位算法
+   //  var gray =(R*76+G*151+B*28)>>8;
+  //平均值算法
+  //   var gray = (R+G+B)/3;
+  //仅取绿色
+   //  var gray=G;
+    imgData.data[i] = gray;
+    imgData.data[i+1] = gray; 
+    imgData.data[i+2] = gray; 
+    imgData.data[i+3] = Alpha; 
+  }
+ ctx.putImageData(imgData,0,0);
+ }
+ 
+ 
+ 
+//调整图片亮度、明度
+ function AdjustBrightness(){
+   var a = 1;
+   var b = document.getElementById("Brightness").value;   //获取用户输入的数据
+   
+   var c_old = document.getElementById("canvas");
+   var c_oldtx = c_old.getContext("2d");
+   var imgData = c_oldtx.getImageData(0,0,c_w,c_h);       //获取图像矩阵
+   
+   //var c_new = document.getElementById("canvas");
+   //var ctx = c_new.getContext("2d");
+   
+   var img = imgData;
+   for (var i=0;i<img.data.length;i+=4){
+     var R = img.data[i]; //R(0-255)
+     var G = img.data[i+1]; //G(0-255)
+     var B = img.data[i+2]; //G(0-255)
+ 
+     img.data[i] = R * a + b;
+     img.data[i+1] = G * a + b;
+     img.data[i+2] = B * a + b; 
+   }
+    c_oldtx.putImageData(img,0,0);
+}
+  
+ 
+ 
---
+++ picApic/js/index.js
@@ -0,0 +1,123 @@
+//读取照片
+document.getElementById("file1").onchange=function(){
+				//1 创建文件读取对象
+				var reader=new FileReader();
+				
+				//文件存储在file表单元素的files属性中，它是一个数组
+				//没有返回值，但是读取完毕后，将读取结果存储在对象的result中
+				var fil=document.getElementById("file1").files;
+				reader.readAsDataURL(fil[0]);
+				reader.readas
+				//当读取成功后触发
+				reader.onload=function(){
+					var url = reader.result;
+					$("#image").css("background-image", "url(" + url + ")");
+					var imageObj = new Image();
+					//imageObj.src = "https://i.loli.net/2021/10/08/bjZvApBqTz3owIR.jpg";
+					imageObj.src = reader.result;
+					imageObj.onload = function(){
+						w = imageObj.width;
+						h = imageObj.height;
+						var canvas = document.createElement('canvas');
+										
+						// Size the canvas to the element
+						canvas.width = w;
+						canvas.height = h;
+										
+						// Draw image onto the canvas
+						var ctx = canvas.getContext('2d');
+						ctx.drawImage(imageObj, 0, 0);
+
+						var imgData = ctx.getImageData(0, 0, w, h);
+						CalculateGray(imgData);
+						
+				    };
+				};
+								
+};
+//计算亮度、灰度
+function CalculateGray(imgData){
+	var gray = 0;
+	for (var i = 0 ; i < imgData.data.length ; i+=4){      //遍历图像矩阵
+	   var R = imgData.data[i]; //R(0-255)
+	   var G = imgData.data[i+1]; //G(0-255)
+	   var B = imgData.data[i+2]; //G(0-255)
+	   var Alpha = imgData.data[i+3]; //Alpha(0-255)
+	   //浮点算法
+	   gray += R*0.299 + G*0.587 + B*0.114;
+	  }
+	alert(gray/imgData.data.length);
+}
+//计算透明度
+ function CalculateAlpha(imgData){
+ 	 var sum = 0;
+ 	 for (var i = 0 ; i < imgData.data.length ; i+=4){
+ 	    var R = imgData.data[i]; //R(0-255)
+ 	    var G = imgData.data[i+1]; //G(0-255)
+ 	    var B = imgData.data[i+2]; //G(0-255)
+ 	    var Alpha = imgData.data[i+3]; //Alpha(0-255)
+	    sum += Alpha;
+ 	  }
+ 	  window.alert(sum/imgData.data.length);
+ }
+
+$(function() {
+    $("#reset").click(function() {
+
+        $("#grayscale").val(0);
+        $("#blur").val(0);
+        $("#brightness").val(100);
+        $("#contrast").val(100);
+        $("#huerotate").val(0);
+        $("#invert").val(0);
+        $("#opacity").val(100);
+        $("#saturate").val(1);
+        $("#sepia").val(0);
+        $("#image").css("-webkit-filter", "none");
+        $("#image").css("-moz-filter", "none");
+    });
+});
+
+$(document).ready(function() {
+
+
+    $("input").on("input", function() {
+        var grayscale = $("#grayscale").val(),
+            blur = $("#blur").val(),
+            brightness = $("#brightness").val(),
+            contrast = $("#contrast").val(),
+            huerotate = $("#huerotate").val(),
+            invert = $("#invert").val(),
+            opacity = $("#opacity").val(),
+            saturate = $("#saturate").val(),
+            sepia = $("#sepia").val();
+		
+
+        $("#image").css({
+            "-webkit-filter": "grayscale(" + grayscale + "%)" +
+                "blur(" + blur + "px)" + "brightness(" + brightness + "%)" +
+                " contrast(" + contrast + "%)" +
+                " hue-rotate(" + huerotate + "deg)" +
+                " invert(" + invert + "%)" +
+                " opacity(" + opacity + "%)" +
+                " saturate(" + saturate + ")" +
+                " sepia(" + sepia + "%)",
+
+            "filter": "grayscale(" + grayscale + "%)" +
+                "blur(" + blur + "px)" + "brightness(" + brightness + "%)" +
+                " contrast(" + contrast + "%)" +
+                " hue-rotate(" + huerotate + "deg)" +
+                " invert(" + invert + "%)" +
+                " opacity(" + opacity + "%)" +
+                " saturate(" + saturate + ")" +
+                " sepia(" + sepia + "%)"
+        });
+
+    });
+	
+//放大图片
+    $("#image").click(function() {
+        $("#image").toggleClass("zoomed");
+
+    });
+});
