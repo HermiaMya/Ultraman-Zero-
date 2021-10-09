75951d285b6797d01fb79eb556f44a99a0b825cb
yyyuanll <91445230+yyyuanll@users.noreply.github.com>
2021-10-09 22:32:37 +0800

    Add files via upload

--- picApic/js/index.js
+++ picApic/js/index.js
@@ -1,39 +1,36 @@
 //读取照片
 document.getElementById("file1").onchange=function(){
-				//1 创建文件读取对象
-				var reader=new FileReader();
+			//1 创建文件读取对象
+			var reader=new FileReader();
 				
-				//文件存储在file表单元素的files属性中，它是一个数组
-				//没有返回值，但是读取完毕后，将读取结果存储在对象的result中
-				var fil=document.getElementById("file1").files;
-				reader.readAsDataURL(fil[0]);
-				reader.readas
-				//当读取成功后触发
-				reader.onload=function(){
-					var url = reader.result;
-					$("#image").css("background-image", "url(" + url + ")");
-					var imageObj = new Image();
-					//imageObj.src = "https://i.loli.net/2021/10/08/bjZvApBqTz3owIR.jpg";
-					imageObj.src = reader.result;
-					imageObj.onload = function(){
-						w = imageObj.width;
-						h = imageObj.height;
-						var canvas = document.createElement('canvas');
+			//文件存储在file表单元素的files属性中，它是一个数组
+			//没有返回值，但是读取完毕后，将读取结果存储在对象的result中
+			var fil=document.getElementById("file1").files;
+			reader.readAsDataURL(fil[0]);
+			reader.readas
+			//当读取成功后触发
+			reader.onload=function(){
+				var url = reader.result;
+				$("#image").css("background-image", "url(" + url + ")");
+				var imageObj = new Image();
+				//imageObj.src = "https://i.loli.net/2021/10/08/bjZvApBqTz3owIR.jpg";
+				imageObj.src = reader.result;
+				imageObj.onload = function(){
+					w = imageObj.width;
+					h = imageObj.height;
+					var canvas = document.createElement('canvas');
 										
-						// Size the canvas to the element
-						canvas.width = w;
-						canvas.height = h;
+					// Size the canvas to the element
+					canvas.width = w;
+					canvas.height = h;
 										
-						// Draw image onto the canvas
-						var ctx = canvas.getContext('2d');
-						ctx.drawImage(imageObj, 0, 0);
-
-						var imgData = ctx.getImageData(0, 0, w, h);
-						CalculateGray(imgData);
-						
-				    };
-				};
-								
+					// Draw image onto the canvas
+					var ctx = canvas.getContext('2d');
+					ctx.drawImage(imageObj, 0, 0);
+					var imgData = ctx.getImageData(0, 0, w, h);
+					CalculateGray(imgData);
+					   };
+				};						
 };
 //计算亮度、灰度
 function CalculateGray(imgData){
@@ -46,7 +43,9 @@ function CalculateGray(imgData){
 	   //浮点算法
 	   gray += R*0.299 + G*0.587 + B*0.114;
 	  }
-	alert(gray/imgData.data.length);
+	var ans = gray/imgData.data.length;
+	var location=document.getElementById("show");
+	location.value = parseInt(ans);
 }
 //计算透明度
  function CalculateAlpha(imgData){
@@ -60,6 +59,23 @@ function CalculateGray(imgData){
  	  }
  	  window.alert(sum/imgData.data.length);
  }
+ 
+function change1(){ 
+      var grayscale=$("#grayscale").val();
+      var location=document.getElementById("show"); 
+      location.value = parseInt(grayscale); 
+} 
+
+ function change2(){
+       var location=document.getElementById("show"); 
+	   var grayscale = location.value;
+	   $("#grayscale").val(grayscale);
+	   $("#image").css({
+	       "-webkit-filter": "grayscale(" + grayscale + "%)",
+	   
+	       "filter": "grayscale(" + grayscale + "%)" 
+	   });
+ } 
 
 $(function() {
     $("#reset").click(function() {
